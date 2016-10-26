cc0956fe232e53b4106d6c97e298d51bfbb5b416
Michel Lemay <mlemay@gmail.com>
2016-10-26 14:49:41 -0400

    Create spark commands.scala

---
+++ spark commands.scala
@@ -0,0 +1,5 @@
+import org.apache.spark.mllib.feature._
+val inputText = "enwik9"
+val input = sc.textFile(inputText).map(line => line.split(" ").toSeq)
+val word2vec = new Word2Vec().setMinCount(10).setNumIterations(10).setVectorSize(300).setNumPartitions(25)
+val model = word2vec.fit(input)
