58ae0569312aec6c7ad1aed80f9c5510b2240f0c
Michel Lemay <mlemay@gmail.com>
2016-10-26 20:20:33 -0400

    Update spark commands.scala

--- spark commands.scala
+++ spark commands.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.mllib.feature._
 val inputText = "enwik9"
 val input = sc.textFile(inputText).map(line => line.split(" ").toSeq)
-val word2vec = new Word2Vec().setMinCount(10).setNumIterations(10).setVectorSize(300).setNumPartitions(16)
+val word2vec = new Word2Vec().setMinCount(10).setNumIterations(10).setVectorSize(300).setNumPartitions(32)
 val model = word2vec.fit(input)
